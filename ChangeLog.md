1966c64648156b460e8bc8cf460c8d78d3e6e4ff
Carter Hay <clccmh@gmail.com>
2025-07-17 09:25:53 -0500

    Moved to cabal for build system.

--- ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for open-gantt
-
-## Unreleased changes
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
-import Lib
+import qualified MyLib (someFunc)
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "Hello, Haskell!"
+  MyLib.someFunc
